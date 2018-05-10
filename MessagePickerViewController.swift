bf7f0c009783636e38bd3745bce864feebc7b337
Virak Ngauv <twohappypancakes@gmail.com>
2018-05-10 14:35:32 -0400

    Add CocoaPods and RealmSwift

---
+++ MessagePickerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MessagePickerViewController.swift
+//  Night Writer
+//
+//  Created by Virak Ngauv on 4/30/18.
+//  Copyright © 2018 Virak Ngauv. All rights reserved.
+//
+
+import UIKit
+
+class MessagePickerViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
